0b7a588d5a070a46cd57fbb8465916237c944d69
devbittu <devbittu@proton.me>
2023-10-29 17:09:23 +0530

    add short url functionality

--- shorturl/models.py
+++ shorturl/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-# from accounts.models import User
+from accounts.models import User
 
 # Create your models here.
 class ShortUrl(models.Model):
     long_url = models.URLField()
     slug = models.SlugField()
     visits = models.IntegerField(default=0)
-    # creator = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    creator = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Url({self.slug}, {self.long_url})"
