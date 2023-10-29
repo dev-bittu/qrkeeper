3481aa3312fc7caa721bb67375dea1adf4eda9c3
devbittu <devbittu@proton.me>
2023-10-29 17:20:19 +0530

    add unique comstraint to shorturl

--- shorturl/models.py
+++ shorturl/models.py
@@ -4,7 +4,7 @@ from accounts.models import User
 # Create your models here.
 class ShortUrl(models.Model):
     long_url = models.URLField()
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     visits = models.IntegerField(default=0)
     creator = models.ForeignKey(to=User, on_delete=models.CASCADE)
 
